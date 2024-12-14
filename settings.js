70bbeb1a29470e115c91415c0b5137c2a4ba9c81
Prasannapranith <pranithprasanna7@gmail.com>
2024-12-14 06:04:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: +94 74 079 9219
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
